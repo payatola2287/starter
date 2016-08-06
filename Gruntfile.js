a3c7ea5ccc5bf5005a715f9bbc41eb419f03d1cc
Paolo Christian Gallardo <Paolo Christian Gallardo>
2016-08-06 11:30:11 +0800

    EDIT: added grunt-webpack to tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,6 +42,7 @@ module.exports = function(grunt) {
             }
         }
     });
+    grunt.loadNpmTasks('grunt-webpack');
     grunt.loadNpmTasks('grunt-postcss');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks("css-mqpacker");
